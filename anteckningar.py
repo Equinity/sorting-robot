36d4f15180c237274848574964910999a900ff03
heloboyIvanschool <ivsv22@student.bth.se>
2024-02-20 15:35:02 +0100

    anteckningar

---
+++ anteckningar.py
@@ -0,0 +1,20 @@
+from pybricks.hubs import EV3Brick as ev3
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
+from pybricks.parameters import Port, Stop, Direction
+from pybricks.tools import wait
+
+# buttons()
+# Check which buttons on the EV3 Brick are currently pressed.
+# Returns List of pressed buttons.
+# Return type List of Button
+
+# classmethod sound.beep(frequency=500, duration=100, volume=30)
+# ljud/sound
+
+# classmethod sound.beeps(number)
+# Play a number of default beeps with a brief pause in between.
+
+# classmethod sound.file(file_name, volume=100)
+    # file_name (str) – Path to the sound file, including extension.
+    # volume (percentage: %) – Volume of the sound (Default: 100)
+
